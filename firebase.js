db4f325b0343aadcbef48e218db81d00c555e04d
vineet433 <vineetsahoo413@gmail.com>
2026-05-07 12:24:55 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD11wroO2abuMzMw1HeAgZf41k9gpAlY2U",
+    authDomain: "lumet-b0779.firebaseapp.com",
+    projectId: "lumet-b0779",
+    storageBucket: "lumet-b0779.firebasestorage.app",
+    messagingSenderId: "460769040491",
+    appId: "1:460769040491:web:285fccbe9fa35c2758392e"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
